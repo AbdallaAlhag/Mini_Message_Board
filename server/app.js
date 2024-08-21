64df7bbf3cb8e951846fd489e4b115932332374b
Abdalla Alhag <alhagabdalla@gmail.com>
2024-08-20 17:46:42 -0700

    Add basic styling for index page alongside header/footer

--- server/app.js
+++ server/app.js
@@ -1,9 +1,9 @@
 import express from "express";
 import dotenv from 'dotenv';
 import morgan from 'morgan';
-import router  from './routes/index.js';
+import router from './routes/index.js';
 dotenv.config();
-  
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`My first Express app - listening on port ${PORT}!`));
@@ -17,3 +17,7 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/', router)
+
+app.use((req, res) => {
+    res.status(404).render('404', { title: '404' });
+});
